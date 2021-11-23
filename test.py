aa280b4f216f1e66150f2472cf96518f48806413
Kean Liu <kean_liu@trendmicro.com>
2021-11-23 14:26:22 +0800

    modify c1

--- test.py
+++ test.py
@@ -8,8 +8,7 @@ def my_func(name, **kwargs):
     print(kwargs)
 
     #kean add 1
-    #kean add 2
-
+    
 # my_func(1, 2, 3, 4, 5, 6, name = 'jack', age = 18)
 
 person01 = {"name": 'Leon', 'age': 20}
